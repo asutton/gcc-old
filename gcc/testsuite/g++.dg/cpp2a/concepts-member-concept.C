d2c7df4071f2b8ba3c712bd7c376dc4372dfd5dc
Andrew Sutton <asutton@lock3software.com>
2018-09-26 16:07:04 -0700

    Update tests to use new concepts.

--- gcc/testsuite/g++.dg/cpp2a/concepts-member-concept.C
+++ gcc/testsuite/g++.dg/cpp2a/concepts-member-concept.C
@@ -1,9 +1,15 @@
 // { dg-options "-std=c++2a" }
 
+// FIXME: Diagnostics should be better.
+
 struct Base {
   template<typename T>
-    static concept bool D() { return __is_same_as(T, int); } // { dg-error "a concept cannot be a member function" }
+    static concept D = __is_same_as(T, int); // { dg-error "does not name a type" }
 
   template<typename T, typename U>
-    static concept bool E() { return __is_same_as(T, U); } // { dg-error "a concept cannot be a member function" }
+    static concept E = __is_same_as(T, U); // { dg-error "does not name a type" }
 };
+
+// FIXME: Actually trap these.
+
+// { dg-excess-errors "deprecated" }
