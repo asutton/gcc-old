1dfa02cbf550882b36a3b82333b84af136e7abe1
Andrew Sutton <asutton@lock3software.com>
2018-10-14 10:34:26 -0400

    Adding tests for variadic constraints.

--- gcc/testsuite/g++.dg/cpp2a/concepts-requires16.C
+++ gcc/testsuite/g++.dg/cpp2a/concepts-requires16.C
@@ -9,7 +9,17 @@ template<typename... Args>
   requires Integral<Args...>
 void f1();
 
+template<typename... Args>
+  requires Integral<Args>... // { dg-error "parameter packs not expanded|expected unqualified-id" }
+void f2();
+
+template<typename... Args>
+  requires (Integral<Args> && ...)
+void f3() { }
+
 void driver()
 {
   f1<int, int>(); // { dg-error "cannot call function" }
+  f3<int, int>();
+  f3<int, void>(); // { dg-error "cannot call function" }
 }
